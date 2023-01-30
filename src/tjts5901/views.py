8cc50f1652b3c52d5c139b581fbb40a5de59f5d5
Pertti Arvola <arvola.pertti@gmail.com>
2023-01-30 16:31:33 +0000

    started doing item adding feature

--- src/tjts5901/views.py
+++ src/tjts5901/views.py
@@ -4,6 +4,9 @@ Basic views for Application
 """
 
 from flask import Blueprint, render_template, send_from_directory
+from flask import Flask, session, redirect, url_for, escape, request, Response, Markup, make_response
+import json
+import urllib
 
 # Main blueprint.
 bp = Blueprint('views', __name__)
@@ -11,14 +14,12 @@ bp = Blueprint('views', __name__)
 # Blueprint for documentation.
 docs_bp = Blueprint('docs', __name__)
 
-@bp.route("/")
+@bp.route("/", methods=['GET', 'POST'])
 def index() -> str:
-    """
-    Index page.
 
-    """
+    user = request.values.get("user")
+    item = request.values.get("item")
+    price = request.values.get("price")
 
-    # Render template file. Template file is using Jinja2 syntax, and can be passed an arbitrary number
-    # of arguments to be used in the template. The template file is located in the templates directory.
-    html = render_template("index.html.j2", title="TJTS5901 Example! I should be changed.")
+    html = render_template("index.html.j2", user=user, item=item, price=price)
     return html
